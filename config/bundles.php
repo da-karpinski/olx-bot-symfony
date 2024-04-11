e3f90129f75ee01eca08bb0f474083c5042dddd2
Dariusz Karpiński <kontakt@dkarpinski.pl>
2024-04-12 00:04:43 +0200

    Add and config for API Platform with JWT auth

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,7 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];

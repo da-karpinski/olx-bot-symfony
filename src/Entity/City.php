98f3215a7c0a5a4fba237ca2b79818a45bbdf29f
Dariusz Karpiński <kontakt@dkarpinski.pl>
2024-04-14 22:44:14 +0200

    Add ApiResource for City and CountryRegion

--- src/Entity/City.php
+++ src/Entity/City.php
@@ -2,17 +2,47 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\CityRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CityRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(
+            uriTemplate: '/city/{id}',
+            normalizationContext: ['groups' => ['city:view']],
+            security: 'is_granted("'.User::ROLE_ADMIN.'") or is_granted("'.User::ROLE_USER.'")',
+        ),
+        new GetCollection(
+            uriTemplate: '/city',
+            paginationEnabled: true,
+            paginationItemsPerPage: 12,
+            normalizationContext: ['groups' => ['city:list']],
+            security: 'is_granted("'.User::ROLE_ADMIN.'") or is_granted("'.User::ROLE_USER.'")',
+        )
+    ],
+    normalizationContext: ['groups' => ['city:list', 'city:view'], 'enable_max_depth' => true],
+    paginationClientEnabled: true,
+    paginationClientItemsPerPage: true,
+    paginationEnabled: true,
+    paginationItemsPerPage: 12,
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'name' => SearchFilter::STRATEGY_PARTIAL,
+    'region.id' => SearchFilter::STRATEGY_EXACT,
+])]
 class City
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['worker:write'])]
+    #[Groups(['city:view', 'city:list'])]
     private ?int $id = null;
 
     #[ORM\Column]
@@ -20,10 +50,11 @@ class City
 
     #[ORM\ManyToOne(inversedBy: 'cities')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['city:view', 'city:list'])]
     private ?CountryRegion $region = null;
 
     #[ORM\Column(length: 60)]
-    #[Groups(['worker:write'])]
+    #[Groups(['city:view', 'city:list'])]
     private ?string $name = null;
 
     public function getId(): ?int
--- src/Entity/CountryRegion.php
+++ src/Entity/CountryRegion.php
@@ -2,23 +2,50 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\CountryRegionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CountryRegionRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(
+            uriTemplate: '/country-region/{id}',
+            normalizationContext: ['groups' => ['country-region:view']],
+            security: 'is_granted("'.User::ROLE_ADMIN.'") or is_granted("'.User::ROLE_USER.'")',
+        ),
+        new GetCollection(
+            uriTemplate: '/country-region',
+            paginationEnabled: true,
+            paginationItemsPerPage: 12,
+            normalizationContext: ['groups' => ['country-region:list']],
+            security: 'is_granted("'.User::ROLE_ADMIN.'") or is_granted("'.User::ROLE_USER.'")',
+        )
+    ],
+    normalizationContext: ['groups' => ['country-region:list', 'country-region:view'], 'enable_max_depth' => true],
+    paginationClientEnabled: true,
+    paginationClientItemsPerPage: true,
+    paginationEnabled: true,
+    paginationItemsPerPage: 12
+)]
 class CountryRegion
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['country-region:view', 'country-region:list', 'city:view', 'city:list'])]
     private ?int $id = null;
 
     #[ORM\Column]
     private ?int $olxId = null;
 
     #[ORM\Column(length: 60)]
+    #[Groups(['country-region:view', 'country-region:list', 'city:view', 'city:list'])]
     private ?string $name = null;
 
     #[ORM\OneToMany(targetEntity: City::class, mappedBy: 'region')]
